a632755a223361e976df2f64be15a5cf5a2c811e
DoumSof99 <sofiadmn99@gmail.com>
2023-03-26 19:41:24 +0300

    ReadMe num2

--- RazorPage/RazorPage/Program.cs
+++ RazorPage/RazorPage/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using RazorPage.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<PizzaDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPage")));
 
 var app = builder.Build();
 
