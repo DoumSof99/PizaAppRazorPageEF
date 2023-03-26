ab725b944255228eacd87e52b2d86597b5743bd1
DoumSof99 <sofiadmn99@gmail.com>
2023-03-26 20:06:36 +0300

    ReadMe num3

---
+++ RazorPage/RazorPage/Pages/Products/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPage.Data;
+using RazorPage.Models;
+
+namespace RazorPage.Pages.Products
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPage.Data.PizzaDbContext _context;
+
+        public IndexModel(RazorPage.Data.PizzaDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Products != null)
+            {
+                Product = await _context.Products.ToListAsync();
+            }
+        }
+    }
+}
